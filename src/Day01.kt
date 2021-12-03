cfcbe6db168a04366f0254e733ed863593aa1f67
Andrei Bacalu <andrei.bacalu@yopeso.com>
2021-12-03 09:26:20 +0200

    Day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) =
+        input.map { it.toInt() }
+            .zipWithNext { previous, current ->
+                previous < current
+            }.count { it }
+
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
