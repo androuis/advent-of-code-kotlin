c3b1fc0844066eab78ed94aeae52a5cca0ba2a50
Andrei Bacalu <andrei.bacalu@yopeso.com>
2021-12-03 10:20:52 +0200

    Day2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var direction = 0
+        var depth = 0
+        input.map { it.split(" ") }.forEach {
+            when (it[0]) {
+                "forward" -> direction += it[1].toInt()
+                "up" -> depth -= it[1].toInt()
+                "down" -> depth += it[1].toInt()
+            }
+        }
+        return direction * depth
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var direction = 0
+        var depth = 0
+        var aim = 0
+        input.map { it.split(" ") }.forEach {
+            when (it[0]) {
+                "forward" -> {
+                    val value = it[1].toInt()
+                    direction += value
+                    depth += aim * value
+                }
+                "up" -> aim -= it[1].toInt()
+                "down" -> aim += it[1].toInt()
+            }
+        }
+        return direction * depth
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
