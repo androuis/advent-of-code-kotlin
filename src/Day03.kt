8f47daa9260540601d34c7411e331c1db6691e75
Andrei Bacalu <andrei.bacalu@yopeso.com>
2021-12-08 14:43:16 +0200

    Day3 part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,17 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val pair = input.map { it.toCharArray().map { it.toString().toInt() } }
+            .reduce { sums, list -> sums.mapIndexed { index, i -> i + list[index] } }
+            .map { it > input.size / 2 }
+            .fold("" to "") { gamaEpsilonPair, isOneMostCommon ->
+                if (isOneMostCommon) {
+                    gamaEpsilonPair.copy(gamaEpsilonPair.first + "1", gamaEpsilonPair.second + "0")
+                } else {
+                    gamaEpsilonPair.copy(gamaEpsilonPair.first + "0", gamaEpsilonPair.second + "1")
+                }
+            }
+        return pair.first.toInt(2) * pair.second.toInt(2)
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
 }
